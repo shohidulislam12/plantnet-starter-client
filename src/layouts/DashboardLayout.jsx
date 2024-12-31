eacfda5027f4d415546bbfe921af14b1156d8940
unknown <shohidulislamsifat2003@gmail.com>
2024-12-31 19:20:19 +0600

    plant net starter fonend complete

---
+++ src/layouts/DashboardLayout.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/Dashboard/Sidebar/Sidebar'
+import useAuth from '../hooks/useAuth'
+import { AuthContext } from '../providers/AuthProvider'
+
+const DashboardLayout = () => {
+
+  return (
+    <div className='relative min-h-screen md:flex bg-white'>
+      {/* Left Side: Sidebar Component */}
+      <Sidebar />
+      {/* Right Side: Dashboard Dynamic Content */}
+      <div className='flex-1  md:ml-64'>
+        <div className='p-5'>
+          {/* Outlet for dynamic contents */}
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardLayout
